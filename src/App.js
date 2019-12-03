1cc1385e446102c7651bc9c4846b1d501719994f
Ruben <34570952+thepixelboy@users.noreply.github.com>
2019-12-03 08:48:11 +0100

    State vs Props

--- src/App.js
+++ src/App.js
@@ -22,9 +22,7 @@ class App extends Component {
   render() {
       return (
         <div className="App">
-          <CardList monsters={this.state.monsters}>
-
-          </CardList>          
+          <CardList monsters={this.state.monsters} />        
         </div>
       );
   }
