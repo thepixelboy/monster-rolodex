44b35eabc48f84b887814a4103c9ea89de26229f
Ruben <34570952+thepixelboy@users.noreply.github.com>
2019-12-03 19:25:24 +0100

    Deploying Our App

---
+++ build/precache-manifest.a9298288adfa14d3c172e12f1b745835.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0d7236c163af3a5907716e6ae0a639",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "13e4836ffdeac5eb607c",
+    "url": "/monster-rolodex/static/css/main.ed4a76b6.chunk.css"
+  },
+  {
+    "revision": "5b66571292b2cce7e259",
+    "url": "/monster-rolodex/static/js/2.73b508e1.chunk.js"
+  },
+  {
+    "revision": "13e4836ffdeac5eb607c",
+    "url": "/monster-rolodex/static/js/main.3a7e16ff.chunk.js"
+  },
+  {
+    "revision": "20081cdcbe059467e3af",
+    "url": "/monster-rolodex/static/js/runtime-main.23ee13ea.js"
+  }
+]);
