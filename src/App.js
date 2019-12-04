aac0cbc5e30f28c334c2c067c22bc7b87b96d97d
Ruben <34570952+thepixelboy@users.noreply.github.com>
2019-12-04 17:27:14 +0100

    Revisiting VirtualDOM + Unidirectional Data Flow

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
 
-  handleChange = (e) => {
-    this.setState({searchField: e.target.value})
+  onSearchChange = event => {
+    this.setState({searchField: event.target.value})
   }
 
   render() {
@@ -36,7 +36,7 @@ class App extends Component {
           <h1> Monsters Rolodex </h1>
           <SearchBox 
             placeholder='search monsters' 
-            handleChange={this.handleChange} 
+            onSearchChange={this.onSearchChange} 
           />          
           <CardList monsters={filteredMonsters} />        
         </div>
